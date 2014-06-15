dc5568a76368743126f923c24fe29fbc5e10fed3
danielsmink <smink83@gmail.com>
2014-06-15 14:21:14 +0200

    * Added VERSION constant * Added gender switch

--- ngMadLibs/js/app.js
+++ ngMadLibs/js/app.js
@@ -1,7 +1,12 @@
 angular.module('madLibs', [])
+    .constant('VERSION', '1.1')
+    .run(function(VERSION, $rootScope){
+        $rootScope.version = VERSION;
+    })
     .controller('madLibsController', function($scope){
         $scope.input = {
-            femaleName:"female name",
+            gender:"female",
+            femaleName:"name",
             jobTitle:"job title",
             tediousTask:"tedious task",
             dirtyTask:"dirty task",
