ce742e61bb394dd76a73c0aaa9715c31e2b2a58e
danielsmink <smink83@gmail.com>
2014-06-23 15:41:05 +0200

    * Added basic styling * Added hover styles * Multiplication table v3

--- multiplication/myApp.js
+++ multiplication/myApp.js
@@ -18,4 +18,17 @@ angular.module('myApp', [])
 
         $scope.numberLimit = $attrs.initialNumberLimit || 10;
 
+        var activeFactorA, activeFactorB;
+        $scope.setActiveFactors = function(a, b) {
+            activeFactorA = a;
+            activeFactorB = b;
+        };
+
+        $scope.matchesFactor = function (a, b) {
+            return a === activeFactorA || b === activeFactorB;
+        }
+
+        $scope.clearActiveFactors = function() {
+            activeFactorA = activeFactorB = null;
+        };
     });
