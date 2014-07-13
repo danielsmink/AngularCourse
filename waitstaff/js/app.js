8c9d38a6e2e92d7e123745634965d9d38b3ec4fc
danielsmink <smink83@gmail.com>
2014-07-13 16:57:41 +0200

    * Basic application functionality

--- waitstaff/js/app.js
+++ waitstaff/js/app.js
@@ -7,26 +7,58 @@ angular.module('waitstaff', [])
     })
     .controller('waitstaffController', function($scope){
 
-        //Initialize errormessage value
+        //Initialize some values
         $scope.errorMessage = false;
         $scope.formValid = false;
 
+        $scope.chargeSubtotal = 0;
+        $scope.chargeTip = 0;
+        $scope.chargeTotal = 0;
+        $scope.tipTotal = 0;
+        $scope.mealCount = 0;
+        $scope.avgTip = 0;
+
         //Form validation
         $scope.validate = function() {
+            //Form is valid so we calculate all the values for the customer charge
             if($scope.mealForm.$valid) {
-                $scope.formValid = true;
+                $scope.chargeSubtotal = $scope.meal.baseprice * (1 + $scope.meal.taxrate / 100);
+                $scope.chargeTip = $scope.chargeSubtotal * ($scope.meal.tippercentage / 100);
+                $scope.chargeTotal = $scope.chargeSubtotal + $scope.chargeTip;
+
+                // Set totals
+                $scope.tipTotal = $scope.tipTotal + $scope.chargeTip;
+                $scope.mealCount = $scope.mealCount + 1;
+                $scope.avgTip = $scope.tipTotal / $scope.mealCount;
+
+                // Reset the meal form
+                $scope.meal = null;
             } else {
                 $scope.errorMessage = 'Please fill in all the fields';
             }
         };
 
-        //Form reset
+        //Meal form reset
         $scope.cancelMeal = function() {
             $scope.formValid = false;
             $scope.errorMessage = false;
             $scope.meal = null;
         };
+
+        //Application reset
+        $scope.resetApp = function() {
+            $scope.formValid = false;
+            $scope.errorMessage = false;
+            $scope.meal = null;
+            $scope.chargeSubtotal = 0;
+            $scope.chargeTip = 0;
+            $scope.chargeTotal = 0;
+            $scope.tipTotal = 0;
+            $scope.mealCount = 0;
+            $scope.avgTip = 0;
+        };
     })
+    // Directive to validate integers shamelessly copied from the AnuglarJS documentation
     .directive('integer', function() {
         return {
             require: 'ngModel',
