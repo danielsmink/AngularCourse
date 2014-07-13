a40a2c255c6576d54a36e160ff26088c4146fed0
danielsmink <smink83@gmail.com>
2014-07-13 16:03:22 +0200

    * Added form validation * Split into two "views" form and text

--- ngMadLibs/js/app.js
+++ ngMadLibs/js/app.js
@@ -1,19 +1,47 @@
+var INTEGER_REGEXP = /^\-?\d+$/;
+
 angular.module('madLibs', [])
-    .constant('VERSION', '1.1')
+    .constant('VERSION', '1.2')
     .run(function(VERSION, $rootScope){
         $rootScope.version = VERSION;
     })
     .controller('madLibsController', function($scope){
-        $scope.input = {
-            gender:"female",
-            femaleName:"name",
-            jobTitle:"job title",
-            tediousTask:"tedious task",
-            dirtyTask:"dirty task",
-            celebrity:"celebrity",
-            uselessSkill:"useless skill",
-            obnoxiuousCelbertity:"obnoxious celebrity",
-            hugeNumber:"huge number",
-            adjective:"adjective"
-        }
+
+        //Initialize errormessage value
+        $scope.errorMessage = false;
+        $scope.formValid = false;
+
+        //Form validation
+        $scope.validate = function() {
+            if($scope.myForm.$valid) {
+                $scope.formValid = true;
+            } else {
+                $scope.errorMessage = 'Please fill in all the fields';
+            }
+        };
+
+        //Form reset
+        $scope.resetForm = function() {
+            $scope.formValid = false;
+            $scope.errorMessage = false;
+            $scope.input = null;
+        };
     })
+    .directive('integer', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function(viewValue) {
+                    if (INTEGER_REGEXP.test(viewValue)) {
+                        // it is valid
+                        ctrl.$setValidity('integer', true);
+                        return viewValue;
+                    } else {
+                        // it is invalid, return undefined (no model update)
+                        ctrl.$setValidity('integer', false);
+                        return undefined;
+                    }
+                });
+            }
+        };
+    });
