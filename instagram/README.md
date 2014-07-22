1f3e190622eb4b4b5b2e955499f3a7d0cd14ad64
danielsmink <smink83@gmail.com>
2014-07-22 16:41:58 +0200

    * Add CSS minification * Optimized PNG image

--- instagram/README.md
+++ instagram/README.md
@@ -5,7 +5,7 @@ Created for Thinkful's Angular course Capstone assignment for Unit 1.
 ## Structure
 
 Source is located in src and divided in module controller and service. Filenames should indicate what can be found where.
-A Gulp file is provided to create a minified JavaScript file.
+A Gulp file is provided to create minified JavaScript and CSS files.
 
 Usage:
 
--- instagram/gulpfile.js
+++ instagram/gulpfile.js
@@ -5,17 +5,25 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('js', function () {
-    gulp.src(['src/**/module.js', 'src/**/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'));
-  });
+  gulp.src(['src/**/module.js', 'src/**/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
 
-gulp.task('watch', ['js'], function () {
-    gulp.watch('src/**/*.js', ['js']);
-  });
+gulp.task('minify-css', function() {
+  gulp.src('css/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['js', 'minify-css'], function () {
+  gulp.watch('src/**/*.js', ['js']);
+  gulp.watch('css/*.css', ['minify-css']);
+});
