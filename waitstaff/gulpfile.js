9bde9353251cf4355877911960d9377181d59e04
danielsmink <smink83@gmail.com>
2014-08-02 12:32:08 +0200

    * Added package.json and gulpfile * Added jshintrc * Added node_modules to gitignore

---
+++ waitstaff/gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    reload = browserSync.reload;
+
+gulp.task('js', function () {
+  gulp.src('./js/app.js')
+    .pipe(sourcemaps.init())
+    .on('error', handleError)
+    .pipe(concat('app.js'))
+    .on('error', handleError)
+    .pipe(ngAnnotate())
+    .on('error', handleError)
+    .pipe(uglify())
+    .on('error', handleError)
+    .pipe(sourcemaps.write())
+    .on('error', handleError)
+    .pipe(gulp.dest('./dist'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('lint', function() {
+  gulp.src('./js/app.js')
+    .on('error', handleError)
+    .pipe(jshint('./js/.jshintrc'))
+    .on('error', handleError)
+    .pipe(jshint.reporter('jshint-stylish'))
+    .on('error', handleError)
+    .pipe(jshint.reporter('fail'))
+    .on('error', handleError);
+});
+
+gulp.task('watch', function () {
+  browserSync({
+    server: {
+      baseDir: '/Users/Shorkan/Documents/AngularCourse/waitstaff/'
+    }
+  });
+  gulp.watch(['./js/app.js'], ['lint']);
+  gulp.watch(['./js/app.js'], ['js']);
+});
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('default', ['lint', 'js']);
